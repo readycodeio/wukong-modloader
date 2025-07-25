a8eaac5306accd8540b4a6fbecfa8488a9cc7b2d
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2025-07-25 13:58:13 +0200

    Hotfix: do not print error origins in console in release mode

--- ReadyM.Loader.Wukong.Bootstrap/Logging/CustomJsonFormatter.cs
+++ ReadyM.Loader.Wukong.Bootstrap/Logging/CustomJsonFormatter.cs
@@ -62,7 +62,7 @@ internal class CustomJsonFormatter(Guid sessionId) : ConsoleFormatter("custom-js
                 var frame = new StackFrame(skip);
                 var method = frame.GetMethod();
                 var methodName = method?.Name ?? "";
-                if (methodName.EndsWith("LoggingExtensions") || methodName.EndsWith("Logging") || methodName.EndsWith("LogError") || methodName.EndsWith("LogCritical"))
+                if (methodName.EndsWith("LoggingExtensions") || methodName.EndsWith("Logging") || methodName.EndsWith("LogError") || methodName.EndsWith("LogCritical") || methodName.EndsWith("LogNull"))
                 {
                     skip++;
                     continue;
