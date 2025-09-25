fb41129a9c3389cd3aedc815f545b577f32c51fc
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2025-09-25 16:27:19 +0200

    Hotfix: Check if b1cs.ini file exists

--- ReadyM.Loader.Wukong.Managed/ModLoaderSettingsFactory.cs
+++ ReadyM.Loader.Wukong.Managed/ModLoaderSettingsFactory.cs
@@ -13,31 +13,36 @@ public class ModLoaderSettingsFactory(PathSettings pathSettings, ILogger logger)
         var baseDir = Directory.GetCurrentDirectory();
         if (!baseDir.EndsWith("Win64"))
             baseDir = Path.Combine(baseDir, "b1\\Binaries\\Win64");
-        
+
         GetModeFromIniConfig(pathSettings.LoaderDir, out bool useDevelop, out var useReload);
-        
+
         var settings = new ModLoaderSettings(useDevelop, useReload);
 
         return settings;
     }
-    
+
     private void GetModeFromIniConfig(string loaderDir, out bool useDevelop, out bool useReload)
     {
         var developValue = "0";
+
         try
         {
             var iniPath = Path.Combine(loaderDir, "b1cs.ini");
             var fullIniPath = Path.GetFullPath(iniPath);
-            var iniParser = new FileIniDataParser();
-            var iniData = iniParser.ReadFile(fullIniPath);
-            developValue = iniData["Settings"]?.GetKeyData("Develop")?.Value ?? "0";
+
+            if (File.Exists(fullIniPath))
+            {
+                var iniParser = new FileIniDataParser();
+                var iniData = iniParser.ReadFile(fullIniPath);
+                developValue = iniData["Settings"]?.GetKeyData("Develop")?.Value ?? "0";
+            }
         }
         catch (Exception ex)
         {
             logger.LogError(ex, "Error while parsing b1cs.ini");
         }
-        
-        useDevelop = developValue == "1" || developValue == "2";
+
+        useDevelop = developValue is "1" or "2";
         useReload = developValue == "2";
 
         logger.LogDebug("Develop Mode: {DevelopMode}", useDevelop);
